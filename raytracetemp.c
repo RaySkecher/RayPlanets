3f3e6e22162de04758e71a2b885c9fc8ecc4e6ca
PenguinDaBozo <pengen0427@gmail.com>
2025-07-30 18:09:12 -0400

    Update raytracetemp.c

--- raytracetemp.c
+++ raytracetemp.c
@@ -20,7 +20,7 @@
 #define PLANET1_ORBIT_SPEED 1.0f
 #define PLANET2_ORBIT_RADIUS 1.6f  
 #define PLANET2_ORBIT_SPEED 0.6f
-#define CAMERA_ORBIT_RADIUS
+#define CAMERA_ORBIT_RADIUS 4.0f
 
 // Scene dimensions
 #define NUM_SPHERES 3
